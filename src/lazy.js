bd4bc8480653037bac4e40125ea179d733da43e6
qiuliangzz <zzh1148707291@163.com>
2019-05-22 11:41:57 +0800

    图片懒加载

--- src/lazy.js
+++ src/lazy.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import lazyload from 'vue-lazyload'
 
+// 注册
 Vue.use(lazyload, {
   error: require('./assets/images/loading.jpg'),
   loading: require('./assets/images/loading.jpg')
