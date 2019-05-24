da2870c2753335e5eb87e71a8815ee57e7bd2a54
qiuliangzz <zzh1148707291@163.com>
2019-05-24 14:16:51 +0800

    vue-create-api

--- src/utils/create-api.js
+++ src/utils/create-api.js
@@ -1,7 +1,9 @@
 // 一个能够让 Vue 组件通过 API 方式调用的插件
+// 用法：https://php.ctolib.com/article/wiki/96982
 // 应用场景：弹窗可使用
 // 应用条件：全局的DOM，在view以外
 // 使用的弹窗组件要加name属性
+
 import Vue from 'vue'
 import CreateAPI from 'vue-create-api'
 import Toast from '../components/common/Toast'
