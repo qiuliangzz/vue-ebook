a29af2e5928e3f5fee39dd513af8605b475d41fd
qiuliangzz <zzh1148707291@163.com>
2019-06-05 10:53:46 +0800

    功能优化

--- src/components/detail/DetaiTitle.vue
+++ src/components/detail/DetaiTitle.vue
@@ -8,80 +8,78 @@
       <span class="icon-shelf icon" v-if="showShelf" @click="showBookShelf"></span>
       <span class="icon-share" v-else></span>
     </div>
-    <div class="title-text" v-if="title">
-      {{title}}
-    </div>
+    <div class="title-text" v-if="title">{{title}}</div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      title: String,
-      showShelf: {
-        type: Boolean,
-        default: true
-      }
+export default {
+  props: {
+    title: String,
+    showShelf: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      ifHideShadow: true
+    };
+  },
+  methods: {
+    showBookShelf() {
+      this.$router.push("/shelf");
     },
-    data() {
-      return {
-        ifHideShadow: true
-      }
+    showShadow() {
+      this.ifHideShadow = false;
     },
-    methods: {
-      showBookShelf() {
-        this.$router.push('/shelf')
-      },
-      showShadow() {
-        this.ifHideShadow = false
-      },
-      hideShadow() {
-        this.ifHideShadow = true
-      },
-      back() {
-        this.$emit('back')
-      }
+    hideShadow() {
+      this.ifHideShadow = true;
+    },
+    back() {
+      this.$emit("back");
     }
   }
+};
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-  @import "../../assets/styles/global";
-  .detail-title-wrapper {
+@import "../../assets/styles/global";
+.detail-title-wrapper {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: rem(42);
+  box-shadow: 0 rem(2) rem(2) 0 rgba(0, 0, 0, 0.1);
+  font-size: rem(20);
+  color: #666;
+  &.hide-shadow {
+    box-shadow: none;
+  }
+  .title-left-wrapper {
     position: relative;
-    display: flex;
+    z-index: 200;
+    flex: 1;
+    margin-left: rem(15);
+    @include left;
+  }
+  .title-right-wrapper {
+    position: relative;
+    z-index: 200;
+    flex: 1;
+    margin-right: rem(15);
+    @include right;
+  }
+  .title-text {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
     width: 100%;
     height: rem(42);
-    box-shadow: 0 rem(2) rem(2) 0 rgba(0, 0, 0, .1);
-    font-size: rem(20);
-    color: #666;
-    &.hide-shadow {
-      box-shadow: none;
-    }
-    .title-left-wrapper {
-      position: relative;
-      z-index: 200;
-      flex: 1;
-      margin-left: rem(15);
-      @include left;
-    }
-    .title-right-wrapper {
-      position: relative;
-      z-index: 200;
-      flex: 1;
-      margin-right: rem(15);
-      @include right;
-    }
-    .title-text {
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 100;
-      width: 100%;
-      height: rem(42);
-      font-size: rem(16);
-      color: #333;
-      @include center;
-    }
+    font-size: rem(16);
+    color: #333;
+    @include center;
   }
+}
 </style>
